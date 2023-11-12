56bfcc2207fffa2f3883dd49ded7dbfc35bb4731
Mmilosnicki <mateuszmilosnicki@gmail.com>
2023-11-12 13:25:17 +0100

    scalanie galzei

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,21 +1,15 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+from testapp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('osoba/<int:pk>/', views.osoba),
+    path('dodaj_osobe/', views.dodaj_osobe),
+    path('lista_osob/', views.lista_osob),
+    path('lista_osob_filter/', views.lista_osob_filter),
+    path('stanowisko/<int:pk>/', views.stanowisko),
+    path('dodaj_stanowisko/', views.dodaj_stanowisko),
+    path('lista_stanowisk/', views.lista_stanowisk),
 ]
--- mysite/testapp/views.py
+++ mysite/testapp/views.py
@@ -1,3 +1,86 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 
-# Create your views here.
+
+@api_view(['GET', 'DELETE'])
+def osoba(request, pk):
+    try:
+        osoba = Osoba.objects.get(pk=pk)
+    except Osoba.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        osoba = Osoba.objects.get(pk=pk)
+        serializer = OsobaModelSerializer(osoba)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        osoba.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['POST'])
+def dodaj_osobe(request):
+    if request.method == 'POST':
+        serializer = OsobaModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def lista_osob(request):
+    if request.method == 'GET':
+        osoby = Osoba.objects.all()
+        serializer = OsobaModelSerializer(osoby, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def lista_osob_filter(request):
+    if request.method == 'GET':
+        search_param = request.query_params.get('search', '')
+        osoby = Osoba.objects.filter(imie__icontains=search_param)
+        serializer = OsobaModelSerializer(osoby, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'DELETE'])
+def stanowisko(request, pk):
+    try:
+        stanowisko = Stanowisko.objects.get(pk=pk)
+    except Stanowisko.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        stanowisko = Stanowisko.objects.get(pk=pk)
+        serializer = StanowiskoSerializer(stanowisko)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        stanowisko.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['POST'])
+def dodaj_stanowisko(request):
+    if request.method == 'POST':
+        stanowisko = Stanowisko()
+        serializer = StanowiskoSerializer(stanowisko, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def lista_stanowisk(request):
+    if request.method == 'GET':
+        stanowiska = Stanowisko.objects.all()
+        serializer = StanowiskoSerializer(stanowiska, many=True)
+        return Response(serializer.data)
